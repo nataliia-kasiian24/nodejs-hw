8f181ddabea212fb392976dc4b49a6bb47f01c63
Kasiian_Nataliia <natalakasian@gmail.com>
2026-04-08 18:25:37 +0300

    correction models

--- src/models/user.js
+++ src/models/user.js
@@ -25,11 +25,10 @@ const userSchema = new Schema(
   },
 );
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', function () {
   if (!this.username) {
     this.username = this.email;
   }
-  next();
 });
 
 userSchema.methods.toJSON = function () {
